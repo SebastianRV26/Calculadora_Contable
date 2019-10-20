6f91a7c3861ad8ab5ab4a413fb02535a0c35c9f7
dani0105 <danielrojas0105@gmail.com>
2019-10-20 16:19:52 -0600

    Merge Branch

--- DataLayer/PresentationLayer/Main.Designer.cs
+++ DataLayer/PresentationLayer/Main.Designer.cs
@@ -29,13 +29,14 @@
         private void InitializeComponent()
         {
             this.btnManager = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.s = new System.Windows.Forms.Panel();
             this.radbtnDollar = new System.Windows.Forms.RadioButton();
             this.radiobtncolon = new System.Windows.Forms.RadioButton();
-            this.lblMoneyTotal = new System.Windows.Forms.Label();
+            this.lblMoney = new System.Windows.Forms.Label();
             this.lblTotal = new System.Windows.Forms.Label();
             this.btnRealiceCierre = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
+            this.moneyContainer = new System.Windows.Forms.FlowLayoutPanel();
+            this.s.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnManager
@@ -50,19 +51,19 @@
             this.btnManager.UseVisualStyleBackColor = false;
             this.btnManager.Click += new System.EventHandler(this.BtnManager_Click);
             // 
-            // panel1
+            // s
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.panel1.Controls.Add(this.radbtnDollar);
-            this.panel1.Controls.Add(this.radiobtncolon);
-            this.panel1.Controls.Add(this.lblMoneyTotal);
-            this.panel1.Controls.Add(this.lblTotal);
-            this.panel1.Controls.Add(this.btnRealiceCierre);
-            this.panel1.Controls.Add(this.btnManager);
-            this.panel1.Location = new System.Drawing.Point(599, 1);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 449);
-            this.panel1.TabIndex = 1;
+            this.s.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.s.Controls.Add(this.radbtnDollar);
+            this.s.Controls.Add(this.radiobtncolon);
+            this.s.Controls.Add(this.lblMoney);
+            this.s.Controls.Add(this.lblTotal);
+            this.s.Controls.Add(this.btnRealiceCierre);
+            this.s.Controls.Add(this.btnManager);
+            this.s.Location = new System.Drawing.Point(599, 1);
+            this.s.Name = "s";
+            this.s.Size = new System.Drawing.Size(200, 449);
+            this.s.TabIndex = 1;
             // 
             // radbtnDollar
             // 
@@ -88,15 +89,15 @@
             this.radiobtncolon.Text = "₡";
             this.radiobtncolon.UseVisualStyleBackColor = true;
             // 
-            // lblMoneyTotal
+            // lblMoney
             // 
-            this.lblMoneyTotal.AutoSize = true;
-            this.lblMoneyTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMoneyTotal.Location = new System.Drawing.Point(47, 387);
-            this.lblMoneyTotal.Name = "lblMoneyTotal";
-            this.lblMoneyTotal.Size = new System.Drawing.Size(118, 31);
-            this.lblMoneyTotal.TabIndex = 3;
-            this.lblMoneyTotal.Text = "dinero :v";
+            this.lblMoney.AutoSize = true;
+            this.lblMoney.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMoney.Location = new System.Drawing.Point(47, 387);
+            this.lblMoney.Name = "lblMoney";
+            this.lblMoney.Size = new System.Drawing.Size(118, 31);
+            this.lblMoney.TabIndex = 3;
+            this.lblMoney.Text = "dinero :v";
             // 
             // lblTotal
             // 
@@ -119,17 +120,24 @@
             this.btnRealiceCierre.Text = "Realizar cierre";
             this.btnRealiceCierre.UseVisualStyleBackColor = false;
             // 
+            // moneyContainer
+            // 
+            this.moneyContainer.Location = new System.Drawing.Point(0, 1);
+            this.moneyContainer.Name = "moneyContainer";
+            this.moneyContainer.Size = new System.Drawing.Size(593, 449);
+            this.moneyContainer.TabIndex = 2;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.moneyContainer);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.s);
             this.Name = "Main";
             this.Text = "Main";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.s.ResumeLayout(false);
+            this.s.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -137,11 +145,12 @@
         #endregion
 
         private System.Windows.Forms.Button btnManager;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label lblMoneyTotal;
+        private System.Windows.Forms.Panel s;
+        private System.Windows.Forms.Label lblMoney;
         private System.Windows.Forms.Label lblTotal;
         private System.Windows.Forms.Button btnRealiceCierre;
         private System.Windows.Forms.RadioButton radbtnDollar;
         private System.Windows.Forms.RadioButton radiobtncolon;
+        private System.Windows.Forms.FlowLayoutPanel moneyContainer;
     }
 }
