444402455d8e5a0038b6215114993cf61a53f2ad
Sebastian RV <sebasrv26@gmail.com>
2019-10-20 18:30:51 -0600

    Manager

--- DataLayer/PresentationLayer/Program.cs
+++ DataLayer/PresentationLayer/Program.cs
@@ -16,7 +16,10 @@ namespace PresentationLayer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            Main main = new Main();
+            main.Show();
+            Application.Run();
+
         }
     }
 }
